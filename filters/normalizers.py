52e64977b2d64ec62c6f684f084fdcd4568e82a6
Alyson Matheus <alyson1907@gmail.com>
2020-06-14 14:58:05 -0300

    -normalizers.py

--- filters/normalizers.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-# Normalizes an image.
-# params:
-#   img: input image to be normalized
-def normalize(img):
-    img = img.astype(float)
-    return (255 * (img - np.min(img)) / (np.max(img) - np.min(img))).astype(np.uint8)
