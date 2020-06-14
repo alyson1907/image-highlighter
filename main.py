b3d97c0b32a946b9e41d03e7fb028056ca0b4ebe
Alyson Matheus <alyson1907@gmail.com>
2020-06-14 14:58:16 -0300

    +main.py

--- main.py
+++ main.py
@@ -3,25 +3,25 @@ import imageio
 import matplotlib.pyplot as plt
 import numpy as np
 # Project modules
-from filters import filters as rt
-from filters import normalizers as norm
+from filters import filter_appliers as f
+from filters import helpers as hp
 
 def printImg(img):
   plt.rcParams['figure.figsize'] = [7, 7]
   plt.imshow(img, cmap="gray")
   plt.show()
 
-filename = 'border_vehicles.png' #str(input())
+filename = 'cerebral_angiography.jpg' #str(input())
 img = imageio.imread('demo/img/' + filename)
 
 # Handling third dimension of some images
 img = img[:, :, 0] if (len(np.shape(img)) == 3) else img
 
 # Applying sharpening filter
-img_out = rt.smoothing_filter(img, n=5, sigma=0.5)
-img_out = rt.sharpening_filter(img, 2)
-img_out = norm.normalize(img_out)
-imageio.imwrite('out.png', img_out)
+img_out = f.smoothing_filter(img, n=5, sigma=0.5, selected=2)
+img_out = f.edge_filter(img, selected=1)
+img_out = hp.normalize(img_out)
 
 # Saving image
-# img_out = img_out.astype(np.uint8)
+img_out = img_out.astype(np.uint8)
+imageio.imwrite('out.png', img_out)
