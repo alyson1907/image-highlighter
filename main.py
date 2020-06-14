129d253b723edbe9b1fe8ec0a4f663c1f0eb20bb
Alyson Matheus <alyson1907@gmail.com>
2020-06-14 17:56:24 -0300

    +original vs results @main.ipynb

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from filters import filter_appliers as f
 from filters import helpers as hp
 
 # User inputs
-filename = str(input("Enter the image name"))
+filename = str(input("Enter the image name "))
 img = imageio.imread('images/' + filename)
-selected_smoothing = int(input("Smoothing Filter number:"))
-selected_edge = int(input("Edge Filter number:"))
+selected_smoothing = int(input("Smoothing Filter number: "))
+selected_edge = int(input("Edge Filter number: "))
 
 # Handling third dimension of some images
 img = img[:, :, 0] if (len(np.shape(img)) == 3) else img
@@ -19,8 +19,9 @@ img = img[:, :, 0] if (len(np.shape(img)) == 3) else img
 img_out = f.smoothing_filter(img, n=5, sigma=0.5, selected=selected_smoothing)
 img_out = f.edge_filter(img, n=3, sigma=1, selected=selected_edge)
 img_out = hp.normalize(img_out)
-# We apply the scalar if Sobel Operator is selected in order to turn white color more visible
-scalar = 6
+
+# We apply the scalar if Sobel Operator is selected in order to turn white edges more visible
+scalar = 5
 img_out = (img_out * scalar) if (selected_edge == 2) else img_out
 
 # Saving image
