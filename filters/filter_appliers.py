b76c355b78fc3d89939faf831c9cf43ef10cb3b7
Alyson Matheus <alyson1907@gmail.com>
2020-06-14 14:59:45 -0300

    +filter_appliers with Smoothing and Edge filters

---
+++ filters/filter_appliers.py
@@ -0,0 +1,40 @@
+# This file gathers all the methods implemented and exports the "main functions" used to apply each method  
+
+import numpy as np
+from filters import smoothing_methods as sm
+from filters import edge_methods as em
+
+def edge_filter(img, selected=2):
+    """
+    Depending on the selected method, applies an Edge Filter to the image
+
+    Parameters:
+        img: image to apply the filter
+        selected: number of the method
+    """
+    if(selected == 1):
+        return em.laplace_of_gaussian(img, n=4, sigma=1)
+
+    elif(selected == 2):
+        return em.sobel_operator(img)
+    
+    else: print('Not an valid Edge Filter')
+
+    return img
+
+def smoothing_filter(img, n=3, sigma=0.5, selected=1):
+    """
+    Applies the selected Smoothing Filter to the image
+
+    Parameters:
+        img: image to apply the filter
+        n: size of the filter
+        sigma: standard deviation (used by gaussian filter)
+    """
+    if (selected == 1):
+        return sm.median_filter(img, n)
+    
+    elif (selected == 2):
+        return sm.gaussian_filter(img, n, sigma)
+    
+    else: print('Not an valid Smoothing Filter)')
