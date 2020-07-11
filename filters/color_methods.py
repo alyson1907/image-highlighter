34a765651c8936aa152adcd174fc20631e14204f
Alyson Matheus <alyson1907@gmail.com>
2020-07-10 21:51:18 -0300

    +multiple colormaps in colorize() @color_methods.py

--- filters/color_methods.py
+++ filters/color_methods.py
@@ -3,6 +3,14 @@
 import numpy as np
 from skimage.color import rgb2hsv
 from skimage.color import gray2rgb
+from skimage.color import rgba2rgb
+import matplotlib.pyplot as plt
+import filters.helpers as hp
+
+def handle_double_channels_img(img):
+  if (len(np.shape(img)) == 2):
+    img = gray2rgb(img)
+  return img
 
 def change_contrast(img, k=0.035):
   """
@@ -13,10 +21,56 @@ def change_contrast(img, k=0.035):
     k: amount to increase/decrease contrast (greater = more intense)
   """
   # Handling greyscale images
-  if (len(np.shape(img)) == 2):
-    img = gray2rgb(img)
+  img = handle_double_channels_img(img)
 
   # convert the input image to HSV
   img_hsv = rgb2hsv(img)
   img_out = (255 / (1 + np.exp(-k * (img.astype(np.int32) - 127))))
   return img_out
+
+def colorize(img, selected=0):
+  """
+  Applies an colormap to the original image.
+
+  Parameters:
+    img: image to change the contrast
+    selected: the selected colormap to apply
+      - 0. None
+      - 1. Hot
+      - 2. Inferno
+      - 3. Twilight
+      - 4. Rainbow
+      - 5. Jet
+  """
+
+  img = handle_double_channels_img(img)
+
+  cmap = None
+  if (selected == 0):
+    return img
+
+  elif (selected == 1):
+    cmap = plt.cm.hot
+  
+  elif (selected == 2):
+    cmap = plt.cm.inferno
+  
+  elif (selected == 3):
+    cmap = plt.cm.twilight
+  
+  elif (selected == 4):
+    cmap = plt.cm.rainbow
+  
+  elif (selected == 5):
+    cmap = plt.cm.jet
+    
+  x, y, z = np.shape(img)
+  img_out = np.zeros((x, y, 3)).astype(np.uint8)
+
+  # Applying colormap
+  img_out = cmap(img)
+  img_out = rgba2rgb(img_out)
+  img_out = hp.trim_dimension(img_out)
+
+  return img_out
+  
